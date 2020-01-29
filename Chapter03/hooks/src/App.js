0a0f8490fa3283cc7ba7b17fe2485b079c8932e8
robert.lee <robert0714@gmail.com>
2020-01-29 19:00:29 +0800

    adjust version

--- Chapter03/hooks/src/App.js
+++ Chapter03/hooks/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 import Hooks from './components/Hooks';
 import Form from './components/Hooks/Form';
